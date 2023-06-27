63b10951b8a820f29f2122b919abae7c175a8aa3
Aver roes <tamerabdalrazaq@hotmail.com>
2023-06-27 09:27:57 +0300

    edit

--- index.py
+++ index.py
@@ -4,6 +4,6 @@ from tester import test
 
 # test = Jumla('ذُو العَقلِ يَشقَى فِي النَعيمِ بِعَقلِهِي')
 test()
-jumla = Jumla("عَذْلُ العَواذِلِ حَوْلَ قَلبي التائِهِي")
+jumla = Jumla("دَمْعٌ جرَى فقضَى في الرَّبْعِ ما وجَبَا")
 print(jumla.tone)
 print(get_composition(jumla.tone))
